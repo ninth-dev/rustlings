5d3ee6cd8aa4f966d638ad17b17b69cb503963ce
ninth-dev <4069123+ninth-dev@users.noreply.github.com>
2019-11-23 10:34:31 +1100

    more completed exercises

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -10,17 +10,20 @@ use std::sync::Arc;
 use std::thread;
 
 fn main() {
-    let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let numbers: Vec<_> = (0..=100u32).collect();
+    // dbg!(numbers);
+    let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
+        let child_numbers = shared_numbers.clone();
         joinhandles.push(thread::spawn(move || {
             let mut i = offset;
             let mut sum = 0;
+
             while i < child_numbers.len() {
                 sum += child_numbers[i];
-                i += 5;
+                i += 1;
             }
             println!("Sum of offset {} is {}", offset, sum);
         }));
